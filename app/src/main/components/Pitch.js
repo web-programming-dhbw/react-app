58b3b5f66e647943a0c6713573993f1c23bc1aa8
Ethan Kelly <kelly.ethan99@gmail.com>
2019-07-23 09:57:58 +0200

    Added isManager and isOwner check + refactor

--- app/src/main/components/Pitch.js
+++ app/src/main/components/Pitch.js
@@ -2,55 +2,26 @@ import React from 'react';
 import { Card, CardText, CardBody, CardTitle, CardSubtitle, Button, CardFooter, Row, Col, Badge } from 'reactstrap';
 import ShowMore from './ShowMore.js';
 
-export default function Pitch({
-  pitch:
-  {
-    id,
-    title,
-    owner,
-    category,
-    desc,
-    is_matched,
-    sponsor_name,
-    sponsor_email,
-    creation_timestamp,
-    matched_timestamp,
-    resources,
-    owner_email
-  }
-}) {
+export default function Pitch(props) {
   return (
     <Card style={{ marginBottom: "40px", height: "400px", overflow: "auto" }}>
       <CardBody style={{ paddingBottom: "15px" }}>
-        {is_matched ? <Badge color="danger">Matched</Badge> : <span />}
-        <CardTitle style={{ marginBottom: "0px", fontSize: "22px", fontWeight: "bold" }}>{title}</CardTitle>
-        <CardSubtitle style={{ marginBottom: "15px" }} className="text-muted">{category}</CardSubtitle>
-        <CardText style={{ marginBottom: "5px", overflow: "auto", height: "150px", textAlign: "justify" }}>
-          {desc.substring(0, 200) + "..."}
+        {props.pitch.is_matched ? <Badge color="danger">Matched</Badge> : <Badge>Unmatched</Badge>}
+        <CardTitle style={{ marginBottom: "0px", fontSize: "22px", fontWeight: "bold" }}>{props.pitch.title}</CardTitle>
+        <CardSubtitle style={{ marginBottom: "15px" }} className="text-muted">{props.pitch.category}</CardSubtitle>
+        <CardText style={{ marginBottom: "5px", overflow: "hidden", height: "130px", textAlign: "justify" }}>
+          {props.pitch.desc.substring(0, 195) + "..."}
         </CardText>
         <Row>
-          <Col><ShowMore pitch={{
-            id,
-            title,
-            owner,
-            category,
-            desc,
-            is_matched,
-            sponsor_name,
-            sponsor_email,
-            creation_timestamp,
-            matched_timestamp,
-            resources,
-            owner_email
-          }} /></Col>
-          <Col>{true ? <Button color='success' size="sm">Offer Sponsorship</Button> : <span />}</Col>
-          <Col>{true ? <Button color='danger' size="sm">Delete Pitch</Button> : <span />}</Col>
+          <Col><ShowMore pitch={props.pitch} isManager={props.isManager} /></Col>
+          {props.isManager && !props.pitch.is_matched ? <Col><Button color='success' size="sm">Offer Sponsorship</Button></Col> : <span />}
+          {props.userEmail === props.pitch.owner_email ? <Col><Button color='danger' size="sm">Delete Pitch</Button></Col> : <span />}
         </Row>
       </CardBody>
       <CardFooter style={{ fontWeight: "bold" }}>
-        {owner}
+        {props.pitch.owner}
         <br />
-        {owner_email}
+        {props.pitch.owner_email}
       </CardFooter>
     </Card>
   );
